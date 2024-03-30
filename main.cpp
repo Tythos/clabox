4b49fc0b6f76dd0374e8602fd029f3b254e15070
Brian Kirkpatrick <bekirkpatrick@proton.me>
2024-03-30 14:23:45 -0700

    let's try this again

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/**
+ * main.cpp
+ */
+
+#include <iostream>
+#include "blaswrap.h"
+
+int main(int nArgs, char** vArgs) {
+    std::cout << "Farewell, cruel world!" << std::endl;
+    return 0;
+}
