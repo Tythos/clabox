8bca781a36e0d70f9835993de9a5c074e07d4b9e
Brian Kirkpatrick <bekirkpatrick@proton.me>
2024-03-30 14:35:31 -0700

    it compiles but does not link

--- main.cpp
+++ main.cpp
@@ -3,9 +3,17 @@
  */
 
 #include <iostream>
+#include "f2c.h"
 #include "blaswrap.h"
+#include "clapack.h"
 
 int main(int nArgs, char** vArgs) {
-    std::cout << "Farewell, cruel world!" << std::endl;
+    long int n = 0;
+    long int incx = 0;
+    long int incy = 0;
+    float sx = 0.0;
+    float sy = 0.0;
+    double out = sdot_(&n, &sx, &incx, &sy, &incy);
+    std::cout << "output: " << out << std::endl;
     return 0;
 }
